82286e4ba7ef496d569d739b5be8489cf81557a0
MCTravisYT <59547361+MDTravisYT@users.noreply.github.com>
2023-12-03 20:48:11 -0500

    remove things

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 @echo off
 
 IF EXIST S1MP.BIN move /Y S1MP.BIN S1MP.BIN.LAST >NUL
-asm68k /k /p /o ae-,c+ S1MP.ASM, S1MP.BIN >S1MP.LOG, , S1MP.LST
+asm68k /k /p /o ae-,c+ SRC/MAIN.ASM, S1MP.BIN >S1MP.LOG, , S1MP.LST
