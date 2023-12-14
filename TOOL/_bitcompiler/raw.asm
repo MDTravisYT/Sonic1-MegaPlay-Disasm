2d9bcdbbd518d20f90c633c50f2052b951abd135
MCTravisYT <59547361+MDTravisYT@users.noreply.github.com>
2023-12-13 19:35:50 -0500

    palette pointers

--- TOOL/_bitcompiler/raw.asm
+++ TOOL/_bitcompiler/raw.asm
@@ -1,1 +1,128 @@
-;	Insert code you want to compile into a binary here
+unk_2BF8:       dc.b   0                ; DATA XREF: ROM:00002450↑o
+	dc.b   0
+	dc.b   0
+	dc.b   0
+	dc.b   0
+	dc.b   0
+	dc.b   0
+	dc.b   0
+	dc.b   0
+	dc.b   0
+	dc.b   0
+	dc.b   0
+	dc.b  $E
+	dc.b $EE
+	dc.b   0
+	dc.b   0
+	dc.b   0
+	dc.b   0
+	dc.b   0
+	dc.b   0
+	dc.b   0
+	dc.b   0
+	dc.b   0
+	dc.b   0
+	dc.b   0
+	dc.b   0
+	dc.b   0
+	dc.b   0
+	dc.b   0
+	dc.b   0
+	dc.b   0
+	dc.b   0
+	dc.b   0
+	dc.b   0
+	dc.b   0
+	dc.b   0
+	dc.b   0
+	dc.b   0
+	dc.b   0
+	dc.b   0
+	dc.b   0
+	dc.b   0
+	dc.b   0
+	dc.b   0
+	dc.b   0
+	dc.b $EE
+	dc.b   0
+	dc.b   0
+	dc.b   0
+	dc.b   0
+	dc.b   0
+	dc.b   0
+	dc.b   0
+	dc.b   0
+	dc.b   0
+	dc.b   0
+	dc.b   0
+	dc.b   0
+	dc.b   0
+	dc.b   0
+	dc.b   0
+	dc.b   0
+	dc.b   0
+	dc.b   0
+	dc.b   4
+	dc.b   0
+	dc.b   0
+	dc.b   0
+	dc.b  $A
+	dc.b $AA
+	dc.b   0
+	dc.b   2
+	dc.b   0
+	dc.b   4
+	dc.b   0
+	dc.b $28 ; (
+	dc.b   0
+	dc.b $4A ; J
+	dc.b   0
+	dc.b $8A
+	dc.b   8
+	dc.b   0
+	dc.b  $A
+	dc.b $42 ; B
+	dc.b  $A
+	dc.b $86
+	dc.b  $A
+	dc.b $A8
+	dc.b   0
+	dc.b   0
+	dc.b   0
+	dc.b $20
+	dc.b   0
+	dc.b $60 ; `
+	dc.b   0
+	dc.b $A4
+	dc.b   8
+	dc.b $40 ; @
+	dc.b   6
+	dc.b   0
+	dc.b   8
+	dc.b   0
+	dc.b  $A
+	dc.b $42 ; B
+	dc.b  $A
+	dc.b $86
+	dc.b  $A
+	dc.b $A8
+	dc.b  $A
+	dc.b $AA
+	dc.b  $A
+	dc.b $68 ; h
+	dc.b  $A
+	dc.b $46 ; F
+	dc.b  $A
+	dc.b $24 ; $
+	dc.b   0
+	dc.b $A4
+	dc.b   0
+	dc.b $60 ; `
+	dc.b   0
+	dc.b   0
+	dc.b   0
+	dc.b   2
+	dc.b   0
+	dc.b $28 ; (
+	dc.b   0
+	dc.b $8A
