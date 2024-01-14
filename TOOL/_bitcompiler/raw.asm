456ec15bf4a516d1fe7a873248007befdadf6bf2
MDTravis <59547361+MDTravisYT@users.noreply.github.com>
2024-01-13 20:50:09 -0500

    finish unused level select code

--- TOOL/_bitcompiler/raw.asm
+++ TOOL/_bitcompiler/raw.asm
@@ -1,320 +1,505 @@
-unk_3196:       dc.b   0
-	dc.b   0
-	dc.b $33 ; 3
-	dc.b   0
-	dc.b   0
-	dc.b   1
-	dc.b   1
-	dc.b   0
-	dc.b   0
-	dc.b   0
-	dc.b $29 ; )
-	dc.b   0
-	dc.b   0
-	dc.b   0
-	dc.b $24 ; $
-	dc.b   0
-	dc.b   0
-	dc.b   1
-	dc.b   1
-	dc.b   0
-	dc.b   0
-	dc.b   1
-	dc.b  $B
-	dc.b   0
-	dc.b   0
-	dc.b   0
-	dc.b $33 ; 3
-	dc.b   0
-	dc.b   0
-	dc.b   0
-	dc.b $2E ; .
-	dc.b   0
-	dc.b   0
-	dc.b   1
-	dc.b $33 ; 3
-	dc.b   0
-	dc.b   0
-	dc.b   2
-	dc.b   1
-	dc.b   0
-	dc.b   0
-	dc.b   1
-	dc.b $29 ; )
-	dc.b   0
-	dc.b   0
-	dc.b   1
-	dc.b $24 ; $
-	dc.b   0
-	dc.b   0
-	dc.b   0
-	dc.b   1
-	dc.b   0
-	dc.b   0
-	dc.b   0
-	dc.b   1
-	dc.b   0
-	dc.b   0
-	dc.b   0
-	dc.b   1
-	dc.b   0
-	dc.b   0
-	dc.b   0
-	dc.b   0
-	dc.b   0
-	dc.b   0
-	dc.b   0
-	dc.b   1
-	dc.b   0
-	dc.b   0
-	dc.b   0
-	dc.b   1
-	dc.b   0
-	dc.b   0
-	dc.b   0
-	dc.b   1
-	dc.b   0
-	dc.b   0
-	dc.b   0
-	dc.b   0
-	dc.b   0
-	dc.b   0
-	dc.b   0
-	dc.b   1
-	dc.b   0
-	dc.b   0
-	dc.b   0
-	dc.b   1
-	dc.b   0
-	dc.b   0
-	dc.b   0
-	dc.b   1
-	dc.b   0
-	dc.b   0
-	dc.b   0
-	dc.b   0
-	dc.b   0
-	dc.b   0
-	dc.b   0
-	dc.b   1
-	dc.b   0
-	dc.b   0
-	dc.b   0
-	dc.b   1
-	dc.b   0
-	dc.b   0
-	dc.b   0
-	dc.b   1
-	dc.b   0
-	dc.b   0
-	dc.b   0
-	dc.b   0
-	dc.b   0
-	dc.b   0
-	dc.b   0
-	dc.b   1
-	dc.b   0
-	dc.b   0
-	dc.b   0
-	dc.b   1
-	dc.b   0
-	dc.b   0
-	dc.b   0
-	dc.b   1
-	dc.b   0
-	dc.b   0
-	dc.b   0
-	dc.b   0
-	dc.b   0
-	dc.b   0
-	dc.b   0
-	dc.b   1
-	dc.b   0
-	dc.b   0
-	dc.b   0
-	dc.b   1
-	dc.b   0
-	dc.b   0
-	dc.b   0
-	dc.b   1
-	dc.b   0
-	dc.b   0
-	dc.b   0
-	dc.b   0
-	dc.b   0
-	dc.b   0
-	dc.b   2
-	dc.b   1
-	dc.b   0
-	dc.b   0
-	dc.b   2
-	dc.b  $B
-	dc.b   0
-	dc.b   0
-	dc.b   1
-	dc.b $33 ; 3
-	dc.b   0
-	dc.b   0
-	dc.b   1
-	dc.b $2E ; .
-	dc.b   0
-	dc.b   0
-	dc.b   2
-	dc.b $1F
-	dc.b   0
-	dc.b   0
-	dc.b   2
-	dc.b $29 ; )
-	dc.b   0
-	dc.b   0
-	dc.b   2
+LevelMenuText:  dc.b $17                ; DATA XREF: LevSelTextLoad↑o
+							; LevSelTextLoad+3E↑o
+	dc.b $22 ; "
 	dc.b $15
-	dc.b   0
-	dc.b   0
-	dc.b   2
+	dc.b $15
+	dc.b $1E
+	dc.b $FF
+	dc.b $18
+	dc.b $19
+	dc.b $1C
+	dc.b $1C
+	dc.b $FF
 	dc.b $10
-	dc.b   0
-	dc.b   0
-	dc.b   4
-	dc.b   1
-	dc.b   0
-	dc.b   0
-	dc.b   4
-	dc.b  $B
-	dc.b   0
-	dc.b   0
-	dc.b   3
-	dc.b $33 ; 3
-	dc.b   0
-	dc.b   0
-	dc.b   3
-	dc.b $2E ; .
-	dc.b   0
-	dc.b   0
-	dc.b   2
-	dc.b  $B
-	dc.b   0
-	dc.b   0
-	dc.b   2
-	dc.b $15
-	dc.b   0
-	dc.b   0
-	dc.b   2
-	dc.b   1
-	dc.b   0
-	dc.b   0
-	dc.b   1
-	dc.b $38 ; 8
-	dc.b   0
-	dc.b   0
-	dc.b   1
-	dc.b $33 ; 3
-	dc.b   0
-	dc.b   0
-	dc.b   2
-	dc.b   1
-	dc.b   0
-	dc.b   0
-	dc.b   1
-	dc.b $29 ; )
-	dc.b   0
-	dc.b   0
-	dc.b   1
-	dc.b $24 ; $
-	dc.b   0
-	dc.b   0
-	dc.b   3
-	dc.b   1
-	dc.b   0
-	dc.b   0
-	dc.b   3
-	dc.b  $B
-	dc.b   0
-	dc.b   0
-	dc.b   2
-	dc.b $33 ; 3
-	dc.b   0
-	dc.b   0
-	dc.b   2
-	dc.b $2E ; .
-	dc.b   0
-	dc.b   0
-	dc.b   3
-	dc.b   1
-	dc.b   0
-	dc.b   0
-	dc.b   3
-	dc.b  $B
-	dc.b   0
-	dc.b   0
-	dc.b   2
-	dc.b $33 ; 3
-	dc.b   0
-	dc.b   0
-	dc.b   2
-	dc.b $2E ; .
-	dc.b   0
-	dc.b   0
-	dc.b   3
 	dc.b $1F
-	dc.b   0
-	dc.b   0
-	dc.b   3
-	dc.b $29 ; )
-	dc.b   0
-	dc.b   0
-	dc.b   3
+	dc.b $1E
 	dc.b $15
-	dc.b   0
-	dc.b   0
-	dc.b   3
-	dc.b $10
-	dc.b   0
-	dc.b   0
-	dc.b   4
+	dc.b $FF
+	dc.b $FF
+	dc.b $23 ; #
+	dc.b $24 ; $
+	dc.b $11
+	dc.b $17
+	dc.b $15
+	dc.b $FF
 	dc.b   1
-	dc.b   0
-	dc.b   0
-	dc.b   4
-	dc.b  $B
-	dc.b   0
-	dc.b   0
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $23 ; #
+	dc.b $24 ; $
+	dc.b $11
+	dc.b $17
+	dc.b $15
+	dc.b $FF
+	dc.b   2
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $23 ; #
+	dc.b $24 ; $
+	dc.b $11
+	dc.b $17
+	dc.b $15
+	dc.b $FF
 	dc.b   3
-	dc.b $33 ; 3
-	dc.b   0
-	dc.b   0
+	dc.b $1D
+	dc.b $11
+	dc.b $22 ; "
+	dc.b $12
+	dc.b $1C
+	dc.b $15
+	dc.b $FF
+	dc.b $10
+	dc.b $1F
+	dc.b $1E
+	dc.b $15
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $23 ; #
+	dc.b $24 ; $
+	dc.b $11
+	dc.b $17
+	dc.b $15
+	dc.b $FF
+	dc.b   1
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $23 ; #
+	dc.b $24 ; $
+	dc.b $11
+	dc.b $17
+	dc.b $15
+	dc.b $FF
+	dc.b   2
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $23 ; #
+	dc.b $24 ; $
+	dc.b $11
+	dc.b $17
+	dc.b $15
+	dc.b $FF
 	dc.b   3
-	dc.b $2E ; .
-	dc.b   0
-	dc.b   0
-	dc.b   9
-	dc.b $3B ; ;
-	dc.b   0
-	dc.b   0
-	dc.b   9
-	dc.b $3B ; ;
-	dc.b   0
-	dc.b   0
-	dc.b   9
-	dc.b $3B ; ;
-	dc.b   0
-	dc.b   0
-	dc.b   9
-	dc.b $3B ; ;
-	dc.b   0
-	dc.b   0
-	dc.b   9
-	dc.b $3B ; ;
-	dc.b   0
-	dc.b   0
-	dc.b   9
-	dc.b $3B ; ;
-	dc.b   0
-	dc.b   0
-	dc.b   9
-	dc.b $3B ; ;
-	dc.b   0
-	dc.b   0
-	dc.b   9
-	dc.b $3B ; ;
-	dc.b   0
+	dc.b $23 ; #
+	dc.b $20
+	dc.b $22 ; "
+	dc.b $19
+	dc.b $1E
+	dc.b $17
+	dc.b $FF
+	dc.b  $F
+	dc.b $11
+	dc.b $22 ; "
+	dc.b $14
+	dc.b $FF
+	dc.b $10
+	dc.b $1F
+	dc.b $1E
+	dc.b $15
+	dc.b $FF
+	dc.b $23 ; #
+	dc.b $24 ; $
+	dc.b $11
+	dc.b $17
+	dc.b $15
+	dc.b $FF
+	dc.b   1
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $23 ; #
+	dc.b $24 ; $
+	dc.b $11
+	dc.b $17
+	dc.b $15
+	dc.b $FF
+	dc.b   2
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $23 ; #
+	dc.b $24 ; $
+	dc.b $11
+	dc.b $17
+	dc.b $15
+	dc.b $FF
+	dc.b   3
+	dc.b $1C
+	dc.b $11
+	dc.b $12
+	dc.b  $F
+	dc.b $22 ; "
+	dc.b $19
+	dc.b $1E
+	dc.b $24 ; $
+	dc.b $18
+	dc.b $FF
+	dc.b $10
+	dc.b $1F
+	dc.b $1E
+	dc.b $15
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $23 ; #
+	dc.b $24 ; $
+	dc.b $11
+	dc.b $17
+	dc.b $15
+	dc.b $FF
+	dc.b   1
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $23 ; #
+	dc.b $24 ; $
+	dc.b $11
+	dc.b $17
+	dc.b $15
+	dc.b $FF
+	dc.b   2
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $23 ; #
+	dc.b $24 ; $
+	dc.b $11
+	dc.b $17
+	dc.b $15
+	dc.b $FF
+	dc.b   3
+	dc.b $23 ; #
+	dc.b $24 ; $
+	dc.b $11
+	dc.b $22 ; "
+	dc.b $FF
+	dc.b $1C
+	dc.b $19
+	dc.b $17
+	dc.b $18
+	dc.b $24 ; $
+	dc.b $FF
+	dc.b $10
+	dc.b $1F
+	dc.b $1E
+	dc.b $15
+	dc.b $FF
+	dc.b $FF
+	dc.b $23 ; #
+	dc.b $24 ; $
+	dc.b $11
+	dc.b $17
+	dc.b $15
+	dc.b $FF
+	dc.b   1
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $23 ; #
+	dc.b $24 ; $
+	dc.b $11
+	dc.b $17
+	dc.b $15
+	dc.b $FF
+	dc.b   2
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $23 ; #
+	dc.b $24 ; $
+	dc.b $11
+	dc.b $17
+	dc.b $15
+	dc.b $FF
+	dc.b   3
+	dc.b $23 ; #
+	dc.b $13
+	dc.b $22 ; "
+	dc.b $11
+	dc.b $20
+	dc.b $FF
+	dc.b $12
+	dc.b $22 ; "
+	dc.b $11
+	dc.b $19
+	dc.b $1E
+	dc.b $FF
+	dc.b $10
+	dc.b $1F
+	dc.b $1E
+	dc.b $15
+	dc.b $FF
+	dc.b $23 ; #
+	dc.b $24 ; $
+	dc.b $11
+	dc.b $17
+	dc.b $15
+	dc.b $FF
+	dc.b   1
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $23 ; #
+	dc.b $24 ; $
+	dc.b $11
+	dc.b $17
+	dc.b $15
+	dc.b $FF
+	dc.b   2
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $23 ; #
+	dc.b $24 ; $
+	dc.b $11
+	dc.b $17
+	dc.b $15
+	dc.b $FF
+	dc.b   3
+	dc.b $16
+	dc.b $19
+	dc.b $1E
+	dc.b $11
+	dc.b $1C
+	dc.b $FF
+	dc.b $10
+	dc.b $1F
+	dc.b $1E
+	dc.b $15
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $23 ; #
+	dc.b $20
+	dc.b $15
+	dc.b $13
+	dc.b $19
+	dc.b $11
+	dc.b $1C
+	dc.b $FF
+	dc.b $23 ; #
+	dc.b $24 ; $
+	dc.b $11
+	dc.b $17
+	dc.b $15
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $23 ; #
+	dc.b $1F
+	dc.b $25 ; %
+	dc.b $1E
+	dc.b $14
+	dc.b $FF
+	dc.b $23 ; #
+	dc.b $15
+	dc.b $1C
+	dc.b $15
+	dc.b $13
+	dc.b $24 ; $
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
+	dc.b $FF
